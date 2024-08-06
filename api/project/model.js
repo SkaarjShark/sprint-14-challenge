2b33258ef39794aea457da4f94d54ecb013dd923
Jacob Handley <skaarjshark@outlook.com>
2024-08-06 15:53:15 -0500

    commit 4. Got the first get request working. Returns empty array because there is nothing in the tables yet.

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,10 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig')
+
+function getProjects() {
+    return db ('project')
+}
+
+module.exports = {
+    getProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -3,7 +3,11 @@ const Project = require('./model')
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-
+    Project.getProjects()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(next)
 })
 
 router.post('/', (req, res, next) => {
