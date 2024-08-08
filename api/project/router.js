69d28a2068a43c3709e3fb08c719b68f65cb8802
Jacob Handley <skaarjshark@outlook.com>
2024-08-08 00:01:59 -0500

    commit 8. Got All the tests happy in the project api.

--- api/project/router.js
+++ api/project/router.js
@@ -4,16 +4,26 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
     Project.getProjects()
-        .then(project => {
-            res.status(200).json(project)
+        .then(projects => {
+            const result = projects.map(project => ({
+                ...project,
+                project_completed: project.project_completed === 1
+            }))
+            res.status(200).json(result)
         })
         .catch(next)
 })
 
 router.post('/', (req, res, next) => {
+    // const newProject = { 
+    //     project_name: req.body.project_name, 
+    //     project_description: req.body.project_description,
+    //     project_completed: req.body.project_completed === undefined ? false : project_completed
+    // }
     Project.createProject(req.body)
         .then(project => {
-            res.status(201).json(project)
+            const result = { ...project, project_completed: project.project_completed === 1}
+            res.status(201).json(result)
         })
         .catch(next)
 })
--- data/seeds/projects-seeds.js
+++ data/seeds/projects-seeds.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
     return knex('projects').insert([
-        { project_id: 1, project_name: 'foo', project_completed: true},
-        { project_id: 2, project_name: 'bar', project_description: 'sample text'}
+        { project_id: 1, project_name: 'foo', project_completed: true },
+        { project_id: 2, project_name: 'bar', project_description: 'sample text', project_completed: false }
     ])
 }
