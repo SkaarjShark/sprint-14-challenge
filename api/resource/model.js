4fdee63e911ceb1baaec973d75bab012645444c6
Jacob Handley <skaarjshark@outlook.com>
2024-08-08 14:57:56 -0500

    commit 9. Get resources endpoint done and tests happy.

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getResources() {
+    return db ('resources')
+}
+
+async function createResource(newResource) {
+    const possibleResource = await db('resources').where('resource_name', newResource.resource_name)
+    if (!possibleResource.length) {
+        const [resource_id] = await db('resources').insert(newResource)
+        return getResources().where({ resource_id }).first()
+    } else {
+        return "nameExists"
+    }
+}
+
+module.exports = {
+    getResources,
+    createResource
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -3,11 +3,23 @@ const Resource = require('./model')
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-
+    Resource.getResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(next)
 })
 
 router.post('/', (req, res, next) => {
-    
+    Resource.createResource(req.body)
+        .then(resource => {
+            if (resource === "nameExists") {
+                res.status(400).json({ message: 'Name already exists in table.' })
+            } else {
+                res.status(201).json(resource)
+            }
+        })
+        .catch(next)
 })
 
 module.exports = router
