0c731928d3268a4f9ab9caee5fbf32a913c473b2
Jacob Handley <skaarjshark@outlook.com>
2024-08-09 18:45:17 -0500

    commit 10. All tests passing, challenge complete. :)

--- api/task/router.js
+++ api/task/router.js
@@ -1,17 +1,28 @@
 // build your `/api/tasks` router here
 const Task = require('./model')
+const Project = require('../project/model')
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
-    Task.getTasks()
-        .then(tasks => {
-            const result = tasks.map(task => ({
-                ...task,
-                task_completed: task.task_completed === 1
-            }))
-            res.status(200).json(result)
-        })
-        .catch(next)
+router.get('/', async (req, res, next) => {
+    try {
+        const gotProjects = await Project.getProjects()
+        Task.getTasks()
+            .then(tasks => {
+                const result = tasks.map(task => {
+                    const project = gotProjects.find(p => p.project_id === task.project_id)
+                    return {
+                        ...task,
+                        task_completed: task.task_completed === 1,
+                        project_name: project.project_name,
+                        project_description: project.project_description
+                    }
+                })
+                res.status(200).json(result)
+            })
+            .catch(next)
+        } catch (err) {
+            res.status(500).json({ error: 'Internal server error '})
+        }
 })
 
 router.post('/', (req, res, next) => {
